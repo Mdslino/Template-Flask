7921cb8c032d2609ad622383b2e5797fb17d1001
Marcelo Lino <mdslino@gmail.com>
2023-01-02 15:54:59 -0300

    Fix tests

--- tests/auth/test_models.py
+++ tests/auth/test_models.py
@@ -4,7 +4,7 @@ from src.ext.database import db
 
 def test_user_exist(auth_user):
     query = db.select(User)
-    user = db.session.execute(query).one()
+    user = db.session.execute(query).scalar_one()
 
     assert auth_user.id == user.id
     assert user.check_password("my_password")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy_utils import create_database, database_exists, drop_database
 
 from src.app import create_app, minimal_app
 from src.ext.database import db
@@ -15,8 +16,14 @@ def min_app():
 def app():
     app = create_app()
     with app.app_context():
+        SQLALCHEMY_DATABASE_URI = app.config["SQLALCHEMY_DATABASE_URI"]
+        if not database_exists(SQLALCHEMY_DATABASE_URI):
+            create_database(SQLALCHEMY_DATABASE_URI)
+        db.session.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+        db.session.commit()
         db.create_all(app=app)
         yield app
+        db.session.close_all()
         db.drop_all(app=app)
 
 
