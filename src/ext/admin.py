4bc23ce9a8081693f70b1128addf350078aaf309
Marcelo Lino <marcelo.dlino@luizalabs.com>
2023-03-02 17:47:50 -0300

    Add Authentication to Admin

--- src/ext/admin.py
+++ src/ext/admin.py
@@ -1,15 +1,36 @@
-from flask_admin import Admin
+from flask import redirect, url_for
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
 
 from src.auth.models import User
 from src.config import settings
 from src.ext.database import db
 
+
+class AuthAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("webui_auth.login"))
+
+
+class AuthModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("webui_auth.login"))
+
+
 admin = Admin(
-    name=settings.TITLE, template_mode=settings.FLASK_ADMIN_TEMPLATE_MODE
+    name=settings.TITLE,
+    template_mode=settings.FLASK_ADMIN_TEMPLATE_MODE,
+    index_view=AuthAdminIndexView(),
 )
 
-admin.add_view(ModelView(User, db.session))
+admin.add_view(AuthModelView(User, db.session))
 
 
 def init_app(app):
